bd0a444b9523aa681655886ae8af9b700976530f
h3shin <h3shin@uwaterloo.ca>
2015-06-22 00:14:07 -0400

    Drew canvas and lines inside that divides it in squares

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+//
+// main.js
+//
+
+function pageLoaded(){
+	// Get a handle to the canvas object
+	var canvas = document.getElementById('testcanvas');
+	// Get the 2d context for this canvas
+	var context = canvas.getContext('2d');
+
+	// Get information from the document
+    var canvaswidth = canvas.getAttribute('width');
+    var canvasheight = canvas.getAttribute('height');
+	var i, numcells = 8;
+    var width = canvaswidth / numcells;
+
+    // Drawing lines dividing the canvas
+	context.beginPath();
+	for(i=0; i < canvaswidth/numcells; ++i) {
+		context.moveTo(i*width,0);
+		context.lineTo(i*width, canvasheight);
+		context.moveTo(0,i*width);
+		context.lineTo(canvaswidth, i*width);
+	}
+	context.closePath();
+	context.stroke();
+}
