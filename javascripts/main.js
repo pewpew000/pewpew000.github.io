a2ecfceac145dd2201b4a933bf6b443e4a32af32
gentlebian <bianhbzju@gmail.com>
2015-07-01 23:38:12 -0400

    start adding data structure

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,22 +17,6 @@ var world_state = new Array(2);	// 2 world_states to reserve local and central's
 
 function drawCharacter(image, r, c, color) {
         context.drawImage(image, r, c, width, width);
-        if(color === "green") {
-	        context.fillStyle = "rgba(0,255,0,0.4)";
-	    }
-	    else if (color === "red") {
-	    	context.fillStyle = "rgba(255,0,0,0.4)";
-	    }
-/*		
-		//left
-		context.fillRect(r-width*2, c, width*2, width);
-		//top
-		context.fillRect(r, c-width*2, width, width*2);
-		//right
-		context.fillRect(r+width, c, width*2, width);
-		//bottom
-		context.fillRect(r, c+width, width, width*2);		
-*/
 }
 
 var imageLoader = {
@@ -65,6 +49,17 @@ function playGame() {
 }
 
 function pageLoaded(){
+	// Draw background grid
+	drawBackground();
+	
+    // Image loading
+	imageLoader.load("images/img_me1.png");
+	imageLoader.load("images/img_me2.png");
+    imageLoader.load("images/img_enemy1.png");
+    imageLoader.load("images/img_enemy2.png");
+}
+
+function drawBackground(){
 	// Get a handle to the canvas object
 	canvas = document.getElementById('testcanvas');
 	// Get the 2d context for this canvas
@@ -88,12 +83,6 @@ function pageLoaded(){
 	}
 	context.closePath();
 	context.stroke();
-
-    // Image loading
-	imageLoader.load("images/img_me1.png");
-	imageLoader.load("images/img_me2.png");
-    imageLoader.load("images/img_enemy1.png");
-    imageLoader.load("images/img_enemy2.png");
 }
 
 /* Writen By Haibo Bian */
@@ -106,6 +95,7 @@ function init(){
 	//initiate listener for user input
 	// move action
 	document.onkeydown = function(event){
+		//alert(event.keyCode);
 		move(images[0], main_coord_x, main_coord_y, "green", event.keyCode);
 	}
 	
@@ -126,28 +116,6 @@ function init(){
 // erease character
 function clearCharacter(image, r, c) {
     context.clearRect( r, c, width, width);
- 
-/* 
-	//left
-	context.clearRect(r-width*2, c, width*2, width);
-	//top
-	context.clearRect(r, c-width*2, width, width*2);
-	//right
-	context.clearRect(r+width, c, width*2, width);
-	//bottom
-	context.clearRect(r, c+width, width, width*2);
-*/	
-	// redraw the erased lines(messy)
-	/*
-		context.beginPath();
-		for(i=0; i < canvas.getAttribute('width')/numcells; ++i) {
-			context.moveTo(i*width,0);
-			context.lineTo(i*width, canvasheight);
-			context.moveTo(0,i*width);
-			context.lineTo(canvaswidth, i*width);
-		}
-		context.closePath();
-	*/
 }
 
 // move character
@@ -174,6 +142,7 @@ function move(image, r, c, color, direction) {
 	
 	// draw new image
 	drawCharacter(image, main_coord_x, main_coord_y, "green");
+	drawBackground();
 }
 
 // draw the fire range
@@ -206,6 +175,55 @@ function ceaseFire(r, c){
 	//bottom
 	context.clearRect(r, c+width, width, width*2);
 	
+	drawBackground();	
 }
 
+/* Data Structures for calculating and judging */
+/*
+// player position
+var position{
+	x = 0;
+	y = 0;
+	set:function(pos_x,pos_y){
+		this.x = pos_x;
+		this.y = pos_y;
+	}
+}
+
+// player death log
+var death_log{
+	death_time  = 0;
+	death_times = 0;
+	add_death:function(time){
+		this.death_time = time;
+		this.death_times++;
+	}
+	have_ever_died:function(){
+		if(this.death_times == 0)
+			return false;
+		return true;
+	}
+}
+
+// world_state
+var world_state{
+	player_num = 0;
+	player_pos = [];
+	player_logs = [];
+	init:function(num){
+		this.player_num = num;
+		var i;
+		for( i = 0; i < num; i++ ){
+			this.player_pos.push({
+				x: random();
+				y: random();
+			});
+			this.player_logs.push({
+				death_time:  0;
+				death_times: 0;
+			})
+		}
+	}
+}
+*/
 
